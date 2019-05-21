2a8e412e0c9c38bdc729884f79b83e24c032e348
Jamitty <juan.robles1417@gmail.com>
2019-05-20 21:48:54 -0300

    Updates

---
+++ precache-manifest.c4b19b971772d18fa612b71631d63d04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70db996c8c7a83c34df6",
+    "url": "/start-page/static/js/runtime~main.66374c66.js"
+  },
+  {
+    "revision": "8a452525b8c16807a526",
+    "url": "/start-page/static/js/main.007c5d7c.chunk.js"
+  },
+  {
+    "revision": "57912692a93ae380e29b",
+    "url": "/start-page/static/js/2.e69071a8.chunk.js"
+  },
+  {
+    "revision": "8a452525b8c16807a526",
+    "url": "/start-page/static/css/main.30cc09b4.chunk.css"
+  },
+  {
+    "revision": "4c4d91d4cef2d775f35833731097dbd9",
+    "url": "/start-page/index.html"
+  }
+];
